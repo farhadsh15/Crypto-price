7bd0cf2a4224931cd6f0fc010e74787dfa859b9a
Farhad Shamsi <farhadshamsi2200@gmail.com>
2023-08-13 21:33:02 +0330

    finish all project

---
+++ src/components/Landing.js
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from 'react';
+
+// API
+import { getCoin } from '../services/api';
+
+// Components
+import Loader from './Loader';
+import Coin from './Coin';
+
+// Style
+import styles from "./Landing.module.css"
+
+const Landing = () => {
+
+    const [coins, setCoins] = useState([]);
+    const [search, setSearch] = useState("");
+
+    useEffect(() => {
+        const fetchAPI = async () => {
+            const data = await getCoin();
+            setCoins(data )
+        }
+
+        fetchAPI();
+    }, [])
+
+    const searchHandeler = event => {
+        setSearch(event.target.value)
+        // console.log(event)
+    }
+
+    const searchedCoin = coins.filter(coin => coin.name.toLowerCase().includes(search.toLowerCase()))
+
+    return (
+        <>
+            <input type='text' placeholder='Search' value={search} onChange={searchHandeler}  className={styles.input} />   
+            {
+                searchedCoin.length ?
+                    <div className={styles.coinContainer}>
+                        {
+                            searchedCoin.map(coin => <Coin 
+                                key={coin.id}
+                                name={coin.name}
+                                image={coin.image}
+                                symbol={coin.symbol}
+                                price={coin.current_price}
+                                marketCap={coin.market_cap}
+                                priceChange={coin.price_change_percentage_24h} 
+                            />)
+                        }
+                    </div> :
+                    <Loader />
+            }
+        </>
+    );
+};
+
+export default Landing;
